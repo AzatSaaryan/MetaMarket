e572a8c5b0917cc957654e131c5136f495c35f3a
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-07-06 23:16:52 +0400

    Remove dist folder from Git tracking

--- backend/dist/NFTs/nftRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-import nftController from "./nftController.js";
-import { uploadImageMiddleware } from "../middlewares/uploadImageMiddleware.js";
-const nftRouter = express.Router();
-nftRouter.post("/create-nft", authMiddleware, uploadImageMiddleware, nftController.createNFT);
-export default nftRouter;
--- backend/dist/users/userRouter.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import userController from "./userController.js";
-const userRouter = express.Router();
-userRouter.put("/:id", userController.updateUser);
-export default userRouter;
