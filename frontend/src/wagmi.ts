97eae8cd7a20e0911b16b24febb038b6f9d44002
Azat Saaryan <azat.saaryan2022@gmail.com>
2025-07-31 19:04:03 +0400

    add more networks in wagmi config

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, goerli, optimism, polygon } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "MetaMarket",
   projectId: "demo",
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, goerli, optimism, polygon],
   ssr: true,
 });
